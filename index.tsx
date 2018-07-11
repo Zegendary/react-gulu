c1ab1e5b82fd19c9c0904e093f6199577e04be69
Zegendary <zxw1992513@qq.com>
2018-07-12 01:27:35 +0800

    init Button

---
+++ index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Button } from "./src/Button";
+
+
+ReactDOM.render(<Button text="按钮"/>, document.getElementById("root"));
