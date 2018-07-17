317ca72a83f7ba9f92e96447c8f40e4df31e6af5
Zegendary <zxw1992513@qq.com>
2018-07-18 02:28:48 +0800

    测试成功

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // OTHER PORTIONS AS MENTIONED BEFORE
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": ["ts", "tsx", "js", "jsx", "json", "node"],
+  // Setup Enzyme
+  "setupTestFrameworkScriptFile": "<rootDir>/src/setupTests.ts",
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|scss|less)$": "<rootDir>/src/__mocks__/styleMock.js"
+  }
+}
