a13a9b0d4aca1b7ae9ba23a21c301f546cd1fed0
Zegendary <zxw1992513@qq.com>
2018-07-18 23:37:50 +0800

    优化目录结构

--- __tests__/Button.test.tsx
+++ __tests__/Button.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 
-import Button from '../components/Button/Button'
+import Button from '../src/components/Button/Button'
 
 it('renders the correct text when children is given', () => {
     const button = enzyme.shallow(<Button>你好</Button>);
--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   "moduleFileExtensions": ["ts", "tsx", "js", "jsx", "json", "node"],
   // Setup Enzyme
-  "setupTestFrameworkScriptFile": "<rootDir>/src/setupTests.ts",
+  "setupTestFrameworkScriptFile": "<rootDir>/setupTests.ts",
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/src/__mocks__/fileMock.js",
-    "\\.(css|scss|less)$": "<rootDir>/src/__mocks__/styleMock.js"
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|scss|less)$": "<rootDir>/__mocks__/styleMock.js"
   }
 }
